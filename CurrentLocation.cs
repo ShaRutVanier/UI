c676ec9f0e7c108498a6f2c834f33fa83d7abc9d
TuYolaVanier <36085669+TuYolaVanier@users.noreply.github.com>
2018-04-19 12:46:05 -0400

    Add files via upload

---
+++ CurrentLocation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentLocation : MonoBehaviour
+{
+	List<string> Floor_1 = new List<string> {"Floor 1" , "101" , "102"};
+
+	public Dropdown dropdown;
+	public Text Selected_Room;
+
+	public void Dropdown_IndexChanged(int index)
+	{
+		Selected_Room.text = Floor_1 [index];
+	}
+
+	void Start()
+	{
+		dropdown.AddOptions (Floor_1);
+	}
+
+}
+/*
+	List<string> Floor_1 = new List<string> {"Floor 1" , "101" , "102"};
+		
+	public Dropdown dropdown;
+
+	public Text SelectedRoom;
+
+	public void DropdownIndex(int index)
+	{
+		SelectedRoom.text = Floor_1[index];
+	}
+
+}
+
+*/
---
+++ SwitchScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SwitchScene : MonoBehaviour {
+
+	public void GoScene (string SceneName) {
+		SceneManager.LoadScene(SceneName);
+	}
+}
